82ac426e49773c69c42399b2b67b8af1e5d95838
Moritz Gunz <moritz.gunz@gmail.com>
2014-07-10 23:35:37 +0200

    Lots of work
    - Old commit didn't include all files

---
+++ Source/Core/LightClaw.Engine/Coroutines/CoroutineContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightClaw.Engine.Core;
+
+namespace LightClaw.Engine.Coroutines
+{
+    public class CoroutineContext : ICoroutineContext
+    {
+        private readonly IEnumerable enumerable;
+
+        private IEnumerator enumerator;
+
+        public event EventHandler<SteppedEventArgs> Stepped;
+
+        public bool IsEnabled { get; set; }
+
+        public bool IsFinished { get; private set; }
+
+        public CoroutineContext(Func<IEnumerable> coroutine)
+            : this(coroutine())
+        {
+            Contract.Requires<ArgumentNullException>(coroutine != null);
+        }
+
+        public CoroutineContext(IEnumerable coroutine)
+        {
+            Contract.Requires<ArgumentNullException>(coroutine != null);
+
+            this.enumerable = coroutine;
+        }
+
+        public void Reset()
+        {
+            this.enumerator = this.enumerable.GetEnumerator();
+            this.IsFinished = false;
+        }
+
+        public bool Step()
+        {
+            object current;
+            return this.Step(out current);
+        }
+
+        public bool Step(out object current)
+        {
+            if (this.IsEnabled && !this.IsFinished)
+            {
+                bool result = !(this.IsFinished = !this.enumerator.MoveNext());
+                current = this.enumerator.Current;
+                return result;
+            }
+            else
+            {
+                current = null;
+                return false;
+            }
+        }
+
+        public void Step(TimeSpan timeOut)
+        {
+            Stopwatch st = Stopwatch.StartNew();
+            while (st.Elapsed < timeOut && this.Step()) { }
+        }
+
+        void IUpdateable.Update()
+        {
+            this.Step();
+        }
+
+        private void RaiseStepped(object current, bool result)
+        {
+            EventHandler<SteppedEventArgs> handler = this.Stepped;
+            if (handler != null)
+            {
+                handler(this, new SteppedEventArgs(current, result));
+            }
+        }
+    }
+}
---
+++ Source/Core/LightClaw.Engine/Coroutines/ICoroutineContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightClaw.Engine.Core;
+
+namespace LightClaw.Engine.Coroutines
+{
+    public interface ICoroutineContext : IUpdateable
+    {
+        bool IsEnabled { get; set; }
+
+        bool IsFinished { get; }
+
+        void Reset();
+
+        bool Step();
+
+        bool Step(out object current);
+
+        void Step(TimeSpan timeOut);
+    }
+}
