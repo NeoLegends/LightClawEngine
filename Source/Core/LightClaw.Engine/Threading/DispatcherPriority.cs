8d52da5a738fc1c4b733ac95b630e1b732952f3f
Moritz Gunz <moritz.gunz@gmail.com>
2014-10-14 13:11:58 +0200

    Added missing code comments on DispatcherPriority

--- Source/Core/LightClaw.Engine/Threading/DispatcherPriority.cs
+++ Source/Core/LightClaw.Engine/Threading/DispatcherPriority.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace LightClaw.Engine.Threading
 {
+    /// <summary>
+    /// Represents the priority the <see cref="Dispatcher"/> sorts its invocation queue with.
+    /// </summary>
     public enum DispatcherPriority
     {
+        /// <summary>
+        /// The task to execute has low priority, it runs in the background.
+        /// </summary>
+        /// <remarks>Use this for e.g. Dispose-requests that do not need to be executed immediately.</remarks>
         Background = 0,
 
+        /// <summary>
+        /// The task has normal priority, it's likely to be executed soon.
+        /// </summary>
         Normal = 1,
 
+        /// <summary>
+        /// The task has high priority, it will be amongst the first to be executed on the next round.
+        /// </summary>
         High = 2
     }
 }
